eac9ea37f55dc3d4da49cc1bfa3a1576c3e78ebc
Aleks Pashkevych <alekspashkevych@gmail.com>
2026-05-16 19:41:38 +0300

    done part of job, need to check

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { getNumbers } from './utils';
+import { Pagination } from './components/Pagination';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const items = getNumbers(1, 42).map(n => `Item ${n}`);
+// const perPage: number = 5;
+const total = items.length;
+const pages: number[] = [];
 
 export const App: React.FC = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [perPage, setPerPage] = useState(5);
+  const [totalPages, setTotalPages] = useState(Math.ceil(total / 5));
+
+  for (let i = 0; i < totalPages; i++) {
+    pages.push(i++);
+  }
+
+  const setPages = (value: number) => {
+    pages.length = 0;
+    setPerPage(value);
+    setTotalPages(Math.ceil(total / value));
+  };
+
   return (
     <div className="container">
       <h1>Items with Pagination</h1>
@@ -19,11 +37,40 @@ export const App: React.FC = () => {
           <select
             data-cy="perPageSelector"
             id="perPageSelector"
-            className="form-control">
-            <option value="3">3</option>
-            <option value="5">5</option>
-            <option value="10">10</option>
-            <option value="20">20</option>
+            className="form-control"
+          >
+            <option
+              value="3"
+              onClick={() => {
+                setPages(3);
+              }}
+            >
+              3
+            </option>
+            <option
+              value="5"
+              onClick={() => {
+                setPages(5);
+              }}
+            >
+              5
+            </option>
+            <option
+              value="10"
+              onClick={() => {
+                setPages(10);
+              }}
+            >
+              10
+            </option>
+            <option
+              value="20"
+              onClick={() => {
+                setPages(20);
+              }}
+            >
+              20
+            </option>
           </select>
         </div>
 
@@ -33,71 +80,15 @@ export const App: React.FC = () => {
       </div>
 
       {/* Move this markup to Pagination */}
-      <ul className="pagination">
-        <li className="page-item disabled">
-          <a
-            data-cy="prevLink"
-            className="page-link"
-            href="#prev"
-            aria-disabled="true">
-            «
-          </a>
-        </li>
-        <li className="page-item active">
-          <a data-cy="pageLink" className="page-link" href="#1">
-            1
-          </a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#2">
-            2
-          </a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#3">
-            3
-          </a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#4">
-            4
-          </a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#5">
-            5
-          </a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#6">
-            6
-          </a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#7">
-            7
-          </a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#8">
-            8
-          </a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#9">
-            9
-          </a>
-        </li>
-        <li className="page-item">
-          <a
-            data-cy="nextLink"
-            className="page-link"
-            href="#next"
-            aria-disabled="false">
-            »
-          </a>
-        </li>
-      </ul>
+      <Pagination
+        total={total} // total number of items to paginate
+        perPage={perPage} // number of items per page
+        currentPage={currentPage} /* optional with 1 by default */
+        pages={pages}
+        onPageChange={page => {
+          setCurrentPage(page);
+        }}
+      />
       <ul>
         <li data-cy="item">Item 1</li>
         <li data-cy="item">Item 2</li>
--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,60 @@
-export const Pagination = () => {};
+type Props = {
+  total: number;
+  perPage: number;
+  currentPage: number;
+  pages: number[];
+  onPageChange: (value: number) => void;
+};
+
+export const Pagination: React.FC<Props> = ({
+  total,
+  // perPage,
+  currentPage,
+  pages,
+  onPageChange,
+}) => {
+  return (
+    <ul className="pagination">
+      <li className={`page-item ${currentPage === 1 && 'disabled'}`}>
+        <a
+          data-cy="prevLink"
+          className="page-link"
+          href="#prev"
+          // aria-disabled="true"
+        >
+          «
+        </a>
+      </li>
+      {pages.map((page, index) => {
+        return (
+          <li
+            key={index}
+            className={`page-item ${page === currentPage && 'active'}`}
+          >
+            <a
+              data-cy="pageLink"
+              className="page-link"
+              href={`#${index + 1}`}
+              onClick={() => {
+                onPageChange(index);
+              }}
+            >
+              {index + 1}
+            </a>
+          </li>
+        );
+      })}
+
+      <li className={`page-item ${currentPage === total && 'disabled'}`}>
+        <a
+          data-cy="nextLink"
+          className="page-link"
+          href="#next"
+          // aria-disabled="false"
+        >
+          »
+        </a>
+      </li>
+    </ul>
+  );
+};
